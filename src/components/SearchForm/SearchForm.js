64a6eacd15a46187e0a3ee8344a82b1aaf2c64f3
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-05-18 17:06:38 +0200

    Separate button component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,3 +1,4 @@
+import Button from "../Button/Button";
 import styles from './SearchForm.module.scss'
 import TextInput from '../TextInput/TextInput';
 
@@ -5,7 +6,7 @@ const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
            <TextInput placeholder="Search..." />
-            <button className={styles.button}>Search</button>
+           <Button text="Search" />
         </form>
     );
 };
