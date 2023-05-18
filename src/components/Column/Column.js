4895b0ff0c91563c2454443a69ebde681b052d5d
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-05-18 17:19:33 +0200

    Added column styles

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,8 +2,8 @@ import styles from "./Column.module.scss";
 
 const Column = (props) => {
   return (
-    <article>
-      <h2>{props.title}</h2>
+    <article className={styles.column}>
+      <h2 className={styles.title}>{props.title}</h2>
     </article>
   );
 };
