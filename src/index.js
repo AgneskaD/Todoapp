1e7538f618bd50660f20c1f7db7d7169902ebd38
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-05-22 14:45:19 +0200

    Created store file in redux folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import { createRoot } from "react-dom/client";
 import App from './App';
 import './styles/normalize.scss';
+import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import "./styles/normalize.scss";
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
 const container = document.querySelector("#root");
 
-createRoot(container).render(<App />);
+createRoot(container).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
