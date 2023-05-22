3df4d7f97bda942e6bee47b616664ea53f579416
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-05-22 15:07:20 +0200

    Deleted data and methods from List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,75 +2,10 @@ import styles from './List.module.scss';
 import Column from "./../Column/Column";
 import ColumnForm from "./../ColumnForm/ColumnForm";
 import { useSelector } from "react-redux";
-// import { useState } from "react";
-// import shortid from "shortid";
 
 const List = () => {
   const columns = useSelector((state) => state.columns);
-  console.log(columns);
-  // const [columns, setColumns] = useState([
-  //   {
-  //     id: 1,
-  //     title: "Books",
-  //     icon: "book",
-  //     cards: [
-  //       { id: 1, title: "Eloquent JavaScript." },
-  //       {
-  //         id: 2,
-  //         title: "Interaktywne strony WWW dla każdego.",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Movies",
-  //     icon: "film",
-  //     cards: [
-  //       { id: 1, title: "React - The complete Guide" },
-  //       {
-  //         id: 2,
-  //         title: "The complete JS course",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Framework",
-  //     icon: "code",
-  //     cards: [
-  //       { id: 1, title: "HTML" },
-  //       {
-  //         id: 2,
-  //         title: "CSS + SCSS",
-  //       },
-  //     ],
-  //   },
-  // ]);
-    
-      // const addColumn = (newColumn) => {
-  //   setColumns([
-  //     ...columns,
-  //     {
-  //       id: shortid(),
-  //       title: newColumn.title,
-  //       icon: newColumn.icon,
-  //       cards: [],
-  //     },
-  //   ]);
-  // };
-    
-      // const addCard = (newCard, columnId) => {
-  //   const columnsUpdated = columns.map((column) => {
-  //     if (column.id === columnId)
-  //       return {
-  //         ...column,
-  //         cards: [...column.cards, { id: shortid(), title: newCard.title }],
-  //       };
-  //     else return column;
-  //   });
-    
-      //   setColumns(columnsUpdated);
-  // };
+ 
       
     return (
         <div className={styles.list}>
@@ -84,17 +19,10 @@ const List = () => {
       </p>
       <section className={styles.columns}>
       {columns.map((column) => (
-        <Column
-        key={column.id}
-        id={column.id}
-        title={column.title}
-        icon={column.icon}
-        cards={column.cards}
-        action={addCard}
-      />
+         <Column key={column.id} {...column} />
         ))}
       </section>
-      <ColumnForm action={addColumn} />
+      <ColumnForm />
       </div>
     );
   };
