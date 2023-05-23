e14d78a9828ab7007923a5c5492cc897a5aa0395
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-05-23 21:19:44 +0200

    Created action Add_COLUMN in ColumnForm component

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,15 +1,18 @@
 import styles from "./ColumnForm.module.scss";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import Button from "../Button/Button";
 import TextInput from "../TextInput/TextInput";
 
-const ColumnForm = ({ action }) => {
+const ColumnForm = () => {
+  const dispatch = useDispatch();
+
     const [title, setTitle] = useState("");
     const [icon, setIcon] = useState("");
 
     const handleSubmit = (e) => {
     e.preventDefault();
-    action({ title, icon });
+   dispatch({ type: "ADD_COLUMN", newColumn: { title, icon } });
     setTitle("");
     setIcon("");
   };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,7 @@
-import { initialState } from "./initialState";
 import { createStore } from "redux";
 
 const reducer = (state, action) => {
+  if (action.type === "ADD_COLUMN")
+    return { ...state, columns: [...state.columns, action.newColumn] };
   return state;
 };
-
-const store = createStore(
-  reducer,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-export default store;
