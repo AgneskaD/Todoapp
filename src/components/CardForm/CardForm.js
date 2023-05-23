eee4c57e76dcf4e168596505006ff5bc79d9c19d
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-05-23 21:40:57 +0200

    Added dispatch redux hook to CardForm

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,13 +2,16 @@ import styles from "./CardForm.module.scss";
 import { useState } from "react";
 import Button from "./../Button/Button";
 import TextInput from "./../TextInput/TextInput";
+import { useDispatch } from "react-redux";
 
-const CardForm = ({ action, columnId }) => {
+const CardForm = ({ columnId }) => {
   const [title, setTitle] = useState("");
+
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    action({ title }, columnId);
+    dispatch({ type: "ADD_CARD", payload: { columnId, title } });
     setTitle("");
   };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,25 @@
+import { initialState } from "./initialState";
 import { createStore } from "redux";
-
+import shortid from "shortid";
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "ADD_COLUMN":
-          return {
-            ...state,
-            columns: [...state.columns, { id: shortid(), ...action.payload }],
-          };
-        default:
-          return state;
-      }
+  switch (action.type) {
+    case "ADD_COLUMN":
+      return {
+        ...state,
+        columns: [...state.columns, { id: shortid(), ...action.payload }],
+      };
+    case "ADD_CARD":
+      return {
+        ...state,
+        cards: [...state.cards, { id: shortid(), ...action.payload }],
+      };
+    default:
+      return state;
+  }
 };
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export default store;
