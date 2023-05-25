f224a32b4169e87e980799fa5b34e3c7f99e80c3
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-05-25 23:07:31 +0200

    Added specific path id to links

--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -11,7 +11,7 @@ export const Lists = () => {
     <section className={styles.lists}>
       <h2 className={styles.heading}>Browse lists</h2>
       {lists.map((list) => (
-        <Link key={list.id} to="list" className={styles.listLink}>
+         <Link key={list.id} to={`list/${list.id}`} className={styles.listLink}>
           <h3>{list.title}</h3>
           <p>{list.description}</p>
         </Link>
