753dcb982157e9b238ad213cbd13ffb71b8823cd
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-05-26 11:29:21 +0200

    Added content box in Favourite page

--- src/components/Favourite/Favourite.js
+++ src/components/Favourite/Favourite.js
@@ -1,11 +1,11 @@
-import styles from "../Hero/Hero.module.scss";
+import styles from "../Favourite/Favourite.module.scss";
 import { PageTitle } from "../PageTitle/PageTitle";
 
 export const Favourite = () => {
   return (
     <div className={styles.hero}>
       <PageTitle style={styles.title}>Favourite</PageTitle>
-      <p>A simple to-do app, with lists, columns and card</p>
+      <article className={styles.column}></article>
     </div>
   );
 };
