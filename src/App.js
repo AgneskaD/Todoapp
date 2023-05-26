48370684bc9f1dc0e8b0aaf0144498c26e1373af
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-05-26 15:03:41 +0200

    Renamed Error file to PageNotFound

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { NavBar } from "./components/NavBar/NavBar";
 import { Home } from "./components/Home/Home";
 import { Favourite } from "./components/Favourite/Favourite";
 import { About } from "./components/About/About";
-import { PageNotFound } from "./components/Error/PageNotFound";
+import { PageNotFound } from "./components/PageNotFound/PageNotFound";
 import { List } from "./components/List/List";
 
 const App = () => {
